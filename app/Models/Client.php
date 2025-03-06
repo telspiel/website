8ae382ad1a78db27f7aa709a20a566df6825b839
ravi <ravi.jalhndra@telspiel.com>
2025-03-06 15:01:28 +0530

    set table in client

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+    protected $table = 'Clients';
     protected $fillable = [
         'client_name',
         'title',
